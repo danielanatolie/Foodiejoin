10a7e4e4cf2443fe4027dd4025b2372bbc4aaec0
danielislam <daniel.anatolie@gmail.com>
2017-03-19 01:32:46 -0700

    updates

---
+++ app/src/main/java/com/example/danlam/foodiejoin/createActivity.java
@@ -0,0 +1,13 @@
+package com.example.danlam.foodiejoin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class createActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create);
+    }
+}
