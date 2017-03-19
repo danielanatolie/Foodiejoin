0431031b3353bf2b6c0acdcbadb1fae2c9506e27
danielislam <daniel.anatolie@gmail.com>
2017-03-18 23:30:20 -0700

    fab bug fix + button listeners

--- app/src/main/java/com/example/danlam/foodiejoin/MainActivity.java
+++ app/src/main/java/com/example/danlam/foodiejoin/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,12 +19,27 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+        */
+
+        Button joinButton = (Button) findViewById(R.id.joinButton);
+        //@todo: joinButton listener
+
+        Button createButton = (Button) findViewById(R.id.createButton);
+        createButton.setOnClickListener(new View.OnClickListener() {
+            //@todo: Manipulates the views should segue to a new screen
+            @Override
+            public void onClick(View view) {
+
             }
         });
     }
